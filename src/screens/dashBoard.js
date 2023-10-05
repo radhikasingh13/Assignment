85a1015e9cf80765886ac7ffb723d3f4471e0164
radhikasingh13 <vidhithakur83@gmail.com>
2023-10-05 17:44:01 +0530

    dsd

--- src/screens/dashBoard.js
+++ src/screens/dashBoard.js
@@ -8,18 +8,7 @@ import Tooltip from 'react-native-walkthrough-tooltip';
 import { AnimatedCircularProgress } from 'react-native-circular-progress';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { BarChart } from "react-native-chart-kit";
-
-const items = ["All", 30506, 30507, 30508, 30509, 30600]
-const progress = [{ key: 0, name: 'Jan', height: 120, nhp: 94 }, { key: 1, name: 'Feb', height: 85, nhp: 130 }, { key: 2, name: 'Mar', height: 45, nhp: 170 }, { key: 3, name: 'Apr', height: 123, nhp: 90 }, { key: 4, name: 'May', height: 195, nhp: 20 }, { key: 5, name: 'Jun', height: 45, nhp: 170 }]
-const time = [{name:'3m',key:2}, {name:'6m',key:5}, {name:'1yr',key:11}, {name:'2yrs',key:3}, {name:'Max',key:3}]
-const data = {
-    labels: ["Jan", "Feb", "Mar", "Apr", "May", "June", "Jul", "Aug"],
-    datasets: [
-        {
-            data: [40, 45, 28, 80, 99, 43]
-        }
-    ]
-};
+import { items,time,data,progress } from "../data/data";
 function DashBoard() {
     const [selectedStartDate, setSelectedStartDate] = useState(null)
     function onDateChange(date) {
@@ -52,62 +41,22 @@ function DashBoard() {
                     </View>
                 </View>
                 <View style={{ marginTop: 7, flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-end', height: hp('28%') }}>
-                    {/* <BarChart
-                        // style={graphStyle}
-                        data={data}
-                        width={Dimensions.get('window').width}
-                        height={220}
-                        withHorizontalLabels={false}
-                        // yAxisLabel="$"
-
-                        withInnerLines={false}
-                        showBarTops={false}
-                        chartConfig={{
-                            backgroundColor: "#FFFFFF",
-                            backgroundGradientFrom: "#FFFFFF",
-                            backgroundGradientTo: "#FFFFFF",
-                            fillShadowGradientFrom: "#FFB627",
-                            fillShadowGradientFromOpacity: "#FFB627",
-                            fillShadowGradientToOpacity: "#FFB627",
-                            barRadius: 12,
-                            // decimalPlaces: 2, // optional, defaults to 2dp
-                            color: (opacity = 1) => `rgba(255, 124, 23, 0.97))`,
-                            labelColor: (opacity = 1) => `rgba(255, 124, 23, 0.97))`,
-                            style: {
-                                borderRadius: 16
-                            },
-
-                            propsForDots: {
-                                r: "8",
-                                strokeWidth: "2",
-                                stroke: "#ffa726"
-                            }
-                        }}
-
-                        verticalLabelRotation={0}
-                    /> */}
-
                     <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
                         {progress?.map((item, index) => (
                             <>
                                 <View style={{ marginLeft: 12, height: hp('30%'), width: wp('12%'), paddingVertical: 12, }}>
+                                   
                                     <View style={{ height: item?.nhp, width: wp('12%'), }}></View>
-                                    <TouchableOpacity onPress={() => {setSelected(item?.key);setToolTipVisible(true)}} style={{ height: item?.height, width: wp('12%'), borderRadius: 12, backgroundColor: selected == item?.key ? "#FFB627" : '#FBDEB5' }}>
-
+                                    
+                                    <TouchableOpacity  onPress={() => {setSelected(item?.key);setToolTipVisible(true)}} style={{ height: item?.height, width: wp('12%'), borderRadius: 12, backgroundColor: selected == item?.key ? "#FFB627" : '#FBDEB5' }}>
+                                    {toolTipVisible && selected == item?.key && <TouchableOpacity onPress={()=>setToolTipVisible(false)} style={{padding:5,alignItems:'center',position:'absolute' ,justifyContent:'center',borderRadius:12,borderBottomRightRadius:0, backgroundColor:'white',elevation:12}}>
+                                    <Text style={{ fontSize: 13, lineHeight: 18, color: selected == item?.key ? "#F36821" : "#C7CDDC", fontFamily: 'TTNorms-Medium' }}>{item?.price}$</Text>
+                                    </TouchableOpacity>}
                                     </TouchableOpacity>
                                     <Text style={{ fontSize: 13, lineHeight: 18, color: selected == item?.key ? "#F36821" : "#C7CDDC", fontFamily: 'TTNorms-Medium' }}>{item?.name}</Text>
 
                                 </View>
-                                <Tooltip
-                                    isVisible={toolTipVisible}
-                                    content={<Text style={{ fontFamily: 'Poppins-Regular', color: 'grey', fontSize: 12 }}>{item?.price}$</Text>}
-                                    placement="top"
-                                    onClose={() => setToolTipVisible(false)}
-                                >
-                                    <View style={{ borderRadius: 20, backgroundColor: toolTipVisible ? "#6F7482" : 'white' }} onPress={() => setToolTipVisible(true)}>
-                                        <Ionicons name="information-circle-outline" size={20} color={toolTipVisible == false ? '#6F7482' : 'white'} />
-                                    </View>
-                                </Tooltip></>
+                               </>
 
                         ))}
                     </ScrollView>
@@ -165,11 +114,7 @@ function DashBoard() {
                         height={hp('35%')}
                         width={wp('100%')}
                         onDateChange={onDateChange}
-                        previousComponent={() => {
-                            return (
-                                <Ionicons name="bag" size={24} color='red' />
-                            )
-                        }}
+                        
                         selectedDayColor={'#E9511D'}
                     />
                 </View>
